33ce53190f0e6bc79e686fe15ece0b756b0c0a4c
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-24 11:59:13 -0600

    commit tree 7b0953bf2c0be8023b6f25c4aaacb8ff69f47a1d

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'JSON'
+
+class GetRequester
+
+    def initialize(url_string)
+        @url_string = url_string
+    end
+
+    def get_response_body
+        uri = URI.parse(@url_string)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        parsed_data = JSON.parse(get_response_body)
+    end
+end
